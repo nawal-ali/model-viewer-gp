1e48062b757a27cf79006bd1ae90cdfdac3572b5
nawal-ali <nmaaa3899@gmail.com>
2025-05-09 15:38:00 +0300

    loaded model and added all labels

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Model from './pages/model.jsx'
+function App() {
+
+  return (
+    <>
+    <Model />
+      <h1>Welcome to React</h1>
+    </>
+  )
+}
+
+export default App
